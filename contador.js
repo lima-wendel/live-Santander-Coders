4248719d05bb14b5370a04f3d3bb1d92f2b1a2ba
lima-wendel <wchristofer@gmail.com>
2021-07-08 18:54:30 -0300

    inserindo a lógica do contador

---
+++ contador.js
@@ -0,0 +1,31 @@
+// tempo em segundos
+let sec = 180;
+
+const countDiv = document.getElementById("timer");
+
+const secpass = () => {
+
+    let min = Math.floor(sec/60); //floor retorna o número truncado para baixo
+    let segundosRestantes = sec % 60;
+
+    if (segundosRestantes < 10) {
+        segundosRestantes = "0" + segundosRestantes;
+    }
+    if (min < 10) {
+        min = "0" + min;
+
+    }
+
+    // formatando para 2:59
+    countDiv.innerHTML = min + ":" + segundosRestantes;
+
+    // condição final
+    if (sec > 0) {
+        sec = sec - 1;
+    }
+    else {
+        countDiv.innerHTML = "Acabou!";
+    }
+};
+
+const countDown = setInterval(() => secpass(), 1000);
